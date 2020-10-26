423051929720decbd13e66a645d2a39f03c85130
dylanrattai <817682@mystma.org>
2020-10-26 14:01:00 -0500

    Removed Hello World; Added Intregers

--- helloWorld/src/app/App.java
+++ helloWorld/src/app/App.java
@@ -2,7 +2,32 @@ package app;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        
+
+        int intreger = 1;
+        // \/< change value | set int and value >/\
+        intreger = 2;
+
+        byte superSmallInt = 127; //8 bit max (127)
+        short shortInt = 32000; //16 bit max (32,000)
+        long longInt = 1000000000;
+        double decimal = 1.192347;
+        float smallDecimal = 1.732f;
+        char unicodeCharacter = 'y';
+        boolean trueFalse = true;
+
+
+
+        /* use sysout and tab to make a System.out
+        \/< Removing warning for not using myNumber
+        */
+        System.out.println(intreger);
+        System.out.println(shortInt);
+        System.out.println(longInt);
+        System.out.println(decimal);
+        System.out.println(smallDecimal);
+        System.out.println(unicodeCharacter);
+        System.out.println(trueFalse);
+        System.out.println(superSmallInt);
+
     }
 }
